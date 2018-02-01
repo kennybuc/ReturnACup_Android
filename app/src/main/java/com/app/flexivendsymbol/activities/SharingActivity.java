9c80ebb7118109601a847862a6acff7ebec8e422
Kenny Buchholtz <kenny.buc88@gmail.com>
2018-01-31 22:27:58 -0800

    added printer function to sharing activity

--- app/src/main/java/com/app/flexivendsymbol/activities/SharingActivity.java
+++ app/src/main/java/com/app/flexivendsymbol/activities/SharingActivity.java
@@ -1,11 +1,16 @@
 package com.app.flexivendsymbol.activities;
 
+import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
 
 import com.app.flexivendsymbol.R;
 import com.app.flexivendsymbol.activities.base.BaseActivity;
+import com.app.flexivendsymbol.helpers.UIUtils;
+import com.er.ERusbsdk.UsbController;
 
 public class SharingActivity extends BaseActivity implements View.OnClickListener {
 
@@ -13,10 +18,29 @@ public class SharingActivity extends BaseActivity implements View.OnClickListene
 
     private String code;
 
+    private UsbController usbController;
+    private UsbDevice usbDevice;
+    private Handler usbHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case UsbController.USB_CONNECTED:
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharing);
+
+        // Setup usb controller.
+        usbController = new UsbController(this, usbHandler);
+        connectPrinter();
 
         code = getIntent().getStringExtra(KEY_SYMBOL_CODE);
         TextView tvCode = findViewById(R.id.tvCode);
@@ -38,11 +62,47 @@ public class SharingActivity extends BaseActivity implements View.OnClickListene
     }
 
     private void printToken() {
+        if (checkUsbPermission()) {
+            byte printStatus = usbController.revByte(usbDevice);
+            if (printStatus == 0x38) {
+                UIUtils.showMessage(this, R.string.paper_unavailable);
+                return;
+            }
 
+            byte[] cmd_resume = new byte[4];
+            cmd_resume[0] = 0x1B;
+            cmd_resume[1] = 0x40; // reset command
+            usbController.sendByte(cmd_resume, usbDevice);
+            usbController.sendByte(new byte[]{0x1D, 0x21, 0x22}, usbDevice);
+            usbController.sendMsg(code, "GBK", usbDevice);
+        } else {
+            UIUtils.showMessage(this, R.string.printer_unavailable);
+        }
     }
 
     private void sendToEmail() {
 
+    }
+
+    private void connectPrinter() {
+        usbController.close();
+        usbDevice = usbController.getUsbDev();
+
+        if (usbDevice != null) {
+            if (!(usbController.isHasPermission(usbDevice))) {
+                usbController.getPermission(usbDevice);
+            }
+        }
+    }
+
+    //check access permission?
+    private boolean checkUsbPermission() {
+        if (usbDevice != null) {
+            if (usbController.isHasPermission(usbDevice)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
